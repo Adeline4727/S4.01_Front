c159c594d3d1b770bd959f4cf348d810e4daa725
Adeline Bergeon <Adeline.Bergeon@etu.univ-smb.fr>
2026-03-16 11:42:45 +0100

    Suite page annonce

---
+++ src/components/InfosPrincipalesAnnonce.vue
@@ -0,0 +1,31 @@
+<script setup>
+import {ref, computed, useTemplateRef, onMounted} from 'vue';
+
+const props = defineProps({
+    annonce : {
+        required : true,
+    }
+})
+
+import { useAnnoncesStore } from '@/stores/annonces';
+const store = useAnnoncesStore();
+
+</script>
+
+<template>
+
+<h1>{{ annonce.titreAnnonce }}</h1>
+<h2>{{ annonce.typeHebergementBien.libelleTypeHebergement }} ⸱ {{ annonce.capacitePersonne }} personnes ⸱ {{ annonce.nbChambre }} chambres ⸱ {{ annonce.adresseBien.villeAdresse.nomVille }}</h2>
+<h3>Min. {{  }} nuits, à partir de <h1>{{ annonce.prixPeriodes[0].prix }}€ / nuit</h1></h3>
+<!-- modif prix min pour le calculer et ajouter minimum de nuits-->
+ <ul>
+    <li v-for="paiement in annonce."><!--Ajouter typesPaiement-->
+        {{ paiement.nomPaiement }}<<!--Vérifier ça-->
+    </li>
+ </ul>
+
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/components/VerifDispoAnnonce.vue
@@ -0,0 +1,18 @@
+<script setup>
+import {ref, computed, useTemplateRef, onMounted} from 'vue';
+import DatePicker from './DatePicker.vue';
+//ajouter annonce props
+</script>
+
+<template>
+
+<p>Sélectionnez vos dates de séjour :</p>
+<p><div class="entoure">i</div> La prochaine date d'arrvée disponible est le {{  }}</p><!--A compléter-->
+<div>
+    <div> Arrivée <DatePicker/></div>
+    <div> Départ <DatePicker/></div>
+</div>
+<div class="trait"></div>
+<img src={{ annonce.proprietaireBien.photoProfil.lienurl }} alt="">
+<h1>{{ annonce.proprietaireBien. }}</h1><!--Ajouter pseudo utilisateur-->
+</template>
