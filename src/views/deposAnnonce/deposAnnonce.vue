3971cc79fff4860eccb72b44c0ecde95ab150de1
Bajeclyv <baptiste.poupeau@etu.univ-smb.fr>
2026-03-16 10:05:24 +0100

    depos Annonces modif

--- src/views/deposAnnonce/deposAnnonce.vue
+++ src/views/deposAnnonce/deposAnnonce.vue
@@ -4,6 +4,15 @@ import { ref } from 'vue'
 const isLogin = ref(true)
 const step = ref(1)
 import { RouterLink, RouterView } from 'vue-router'
+
+const types = [
+
+{name:"maison"},
+{name:"appartement"},
+{name:"Autres"},
+
+]
+
 </script>
 
 <template>
@@ -15,10 +24,44 @@ import { RouterLink, RouterView } from 'vue-router'
               <label>Quel est le titre de l'annonce</label>
               <input class="input-element"/>
             </div>
-            <Button @click="step+=1">Suivant</Button>
+            <Button @click="step+=1">Continuer</Button>
         </div>
         <div v-else-if="step == 2">
             <h1>Où se situe votre bien ?</h1>
+            <input class="input-element"/>
+            <Button @click="step-=1">Retour</Button>
+            <Button @click="step+=1">Continuer</Button>
+        </div>
+        <div v-else-if="step == 3">
+            <h1>Dites-nous en plus</h1>
+            <p>Choisissez votre type de bien*</p>
+            <div v-for="type in types">
+                <input type="radio" name="type" :id="type.name">
+                <label :for="type.name">{{type.name}}</label>
+            </div>
+            <div>
+                <h3>Les critères indispensables</h3>
+                <div>
+                    <div class="input-group">
+                        <label>Surface habitable *</label>
+                        <input class="input-element"/>
+                        <p>Comptez les surfaces intérieures habitables d'une hauteur sous plafond de plus de 1,80m.</p>
+                    </div>
+                    <div class="input-group">
+                        <label>Surface habitable *</label>
+                        <input class="input-element"/>
+                    </div>
+                    <div class="input-group">
+                        <label>Surface habitable *</label>
+                        <input class="input-element"/>
+                    </div>
+                </div>
+            </div>
+            <Button @click="step-=1">Retour</Button>
+            <Button @click="step+=1">Continuer</Button>
+        </div>
+        <div v-else-if="step == 4">
+            <Button @click="step-=1">Retour</Button>
         </div>
     </form>
 </template>
