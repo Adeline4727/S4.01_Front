3a5c3579f4b2df44f281b9e47d9280d19d64b568
MatthieuNegri <Matthieu.Negri@etu.univ-smb.fr>
2026-03-23 18:07:39 +0100

    Resevation

--- src/router/index.js
+++ src/router/index.js
@@ -91,7 +91,14 @@ const router = createRouter({
       // which is lazy-loaded when the route is visited.
       component: () => import('../views/Messagerie/Messagerie.vue'),
     },
-    
+    {
+      path: '/reservationAnnonce/:id',
+      name: 'reservationAnnonce',
+      // route level code-splitting
+      // this generates a separate chunk (About.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import('../views/Annonce/ReservationAnnonce.vue'),
+    },
     
   ],
 })
---
+++ src/views/Annonce/ReservationAnnonce.vue
@@ -0,0 +1,26 @@
+<script setup>
+import { computed, onMounted } from 'vue'
+import { useRoute } from 'vue-router'
+import { useAnnoncesStore } from '@/stores/annonces'
+import BandeauResevationAnnonce from '@/components/BandeauResevationAnnonce'
+import BandeauRecapPayementAnnonce from '@/components/BandeauRecapPayementAnnonce'
+
+
+const route = useRoute()
+const store = useAnnoncesStore()
+onMounted(() => {
+    store.getAnnonceById(route.params.id);
+})
+</script>
+
+<template>
+  <div v-if="store.annonce">
+    <BandeauResevationAnnonce :annonce="store.annonce"/>
+    <BandeauRecapPayementAnnonce :annonce="store.annonce"/>
+  </div>
+  <div v-else>Chargement...</div>
+</template>
+
+<style scoped>
+
+</style>
