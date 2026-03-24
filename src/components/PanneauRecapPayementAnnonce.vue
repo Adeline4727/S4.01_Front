388168a189136c433e26cc73411b4d2d8161800c
MatthieuNegri <Matthieu.Negri@etu.univ-smb.fr>
2026-03-24 15:13:39 +0100

    reservationAnnonce fin structure et debut css

--- src/components/PanneauRecapPayementAnnonce.vue
+++ src/components/PanneauRecapPayementAnnonce.vue
@@ -8,62 +8,68 @@ const props = defineProps({
 </script>
 
 <template>
-    <div>
-        <div>
-            <img src="" alt="">
-        </div>
-        <div>
-            <h2></h2>
-            <p></p>
-            <p></p>
-        </div>
-    </div>
-    <div class="trait"></div>
-    <div>
-        <div>
-            <img src="" alt="">
-            <div class="text-body-1 font-bold">Prudhomme Michel</div>
-        </div>
-    </div>
-    <div class="trait"></div>
-    <div>
-        <h2>Récapitulatif du paiement</h2>
+    <div class="PanneauRecapPayementAnnonce">
         <div>
             <div>
-                <p>Montant de la location</p>
-                <p></p>
+                <img src="" alt="">
             </div>
             <div>
-                <p>Frais de service</p>
+                <h2></h2>
                 <p></p>
-            </div>
-            <div>
-                <p>Taxe de séjour</p>
                 <p></p>
             </div>
         </div>
-    </div>
-    <div class="trait"></div>
-    <div>
+        <div class="trait"></div>
         <div>
             <div>
-                <p>Total</p>
-                <p></p>
-            </div>
-            <div>
-                <p>À payer maintenant</p>
-                <p></p>
-            </div>
-            <div>
-                <p>Restera à payer sur place</p>
-                <p></p>
+                <img src="" alt="">
+                <div class="text-body-1 font-bold">Prudhomme Michel</div>
             </div>
         </div>
-        <p>Ce paiement nous sert à garantir votre réservation. Il inclut un acompte pour la location, les frais de service et la taxe de séjour. Retrouvez le détail de nos conditions d’annulation ici</p>
+        <div class="trait"></div>
+        <div>
+            <h2>Récapitulatif du paiement</h2>
+            <div>
+                <div>
+                    <p>Montant de la location</p>
+                    <p></p>
+                </div>
+                <div>
+                    <p>Frais de service</p>
+                    <p></p>
+                </div>
+                <div>
+                    <p>Taxe de séjour</p>
+                    <p></p>
+                </div>
+            </div>
+        </div>
+        <div class="trait"></div>
+        <div>
+            <div>
+                <div>
+                    <p>Total</p>
+                    <p></p>
+                </div>
+                <div>
+                    <p>À payer maintenant</p>
+                    <p></p>
+                </div>
+                <div>
+                    <p>Restera à payer sur place</p>
+                    <p></p>
+                </div>
+            </div>
+            <p>Ce paiement nous sert à garantir votre réservation. Il inclut un acompte pour la location, les frais de service et la taxe de séjour. Retrouvez le détail de nos conditions d’annulation ici</p>
+        </div>
     </div>
 </template>
 
 <style scoped>
+.PanneauRecapPayementAnnonce{
+    display: flex;
+    flex-direction: column;
+}
 .trait{
     height: 1px;
     margin-top: 20px;
