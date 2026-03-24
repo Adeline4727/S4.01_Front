49ffebf81bdc69a21b376be7f2999485a4cfe7da
MatthieuNegri <Matthieu.Negri@etu.univ-smb.fr>
2026-03-24 14:27:15 +0100

    Panneau recap

--- src/components/PanneauRecapPayementAnnonce.vue
+++ src/components/PanneauRecapPayementAnnonce.vue
@@ -8,6 +8,59 @@ const props = defineProps({
 </script>
 
 <template>
+    <div>
+        <div>
+            <img src="" alt="">
+        </div>
+        <div>
+            <h2></h2>
+            <p></p>
+            <p></p>
+        </div>
+    </div>
+    <div class="trait"></div>
+    <div>
+        <div>
+            <img src="" alt="">
+            <div class="text-body-1 font-bold">Prudhomme Michel</div>
+        </div>
+    </div>
+    <div class="trait"></div>
+    <div>
+        <h2>Récapitulatif du paiement</h2>
+        <div>
+            <div>
+                <p>Montant de la location</p>
+                <p></p>
+            </div>
+            <div>
+                <p>Frais de service</p>
+                <p></p>
+            </div>
+            <div>
+                <p>Taxe de séjour</p>
+                <p></p>
+            </div>
+        </div>
+    </div>
+    <div class="trait"></div>
+    <div>
+        <div>
+            <div>
+                <p>Total</p>
+                <p></p>
+            </div>
+            <div>
+                <p>À payer maintenant</p>
+                <p></p>
+            </div>
+            <div>
+                <p>Restera à payer sur place</p>
+                <p></p>
+            </div>
+        </div>
+        <p>Ce paiement nous sert à garantir votre réservation. Il inclut un acompte pour la location, les frais de service et la taxe de séjour. Retrouvez le détail de nos conditions d’annulation ici</p>
+    </div>
 </template>
 
 <style scoped>
