fa0bde776f34cb35b827b244c868f913204d44f8
Adeline Bergeon <Adeline.Bergeon@etu.univ-smb.fr>
2026-03-24 14:43:25 +0100

    ShowAnnonce

--- src/components/ButtonShare.vue
+++ src/components/ButtonShare.vue
@@ -1,42 +1,25 @@
 <script setup>
-const props = defineProps({
-    shared: {
-        type: Boolean,
-        default: false
-    }
-})
-
-const emit = defineEmits(['update:shared'])
-
-function share() {
-    emit('update:shared', !props.shared)
-}
 </script>
 
 <template>
-    <div class="share-button" @click="share">
-        <ion-icon v-if="shared" class="shared" name="heart"></ion-icon>
-        <ion-icon v-else name="heart-outline"></ion-icon>
+    <div class="share-button">
+        <ion-icon name="share-social-outline"></ion-icon>
     </div>
 </template>
 
 <style>
     .share-button {
         background-color: white;
-        width: 1.5rem;
+        width: 1rem;
         font-size: 1rem;
-        border-radius: 50%;
+        border-radius: 30%;
     }
 
     .share-button:hover {
-        background-color: #F5F5F5;
+        background-color: #d1d1d1;
     }
 
     .share-button ion-icon {
         margin-top: 0.25rem;
-    }
-    
-    .share-button ion-icon.shared {
-        color: indianred;
     }
 </style>
--- src/components/PhotosAnnonce.vue
+++ src/components/PhotosAnnonce.vue
@@ -1,9 +1,41 @@
 <script setup>
+import {ref, computed, useTemplateRef, onMounted} from 'vue';
+
+const props = defineProps({
+    annonce : {
+        required : true,
+    }
+})
 
 </script>
 <template>
-
+    <!-- <div v-if="annonce.photos.length==0" class="photos">Cette annonce ne contient pas de photos</div>
+    <div v-else-if="annonce.photos.length==1" class="photos"><img :src="annonce.photos[0].lienurl" alt="" class="unePhoto"></div>
+    <div v-else-if="annonce.photos.length==2" class="photos">
+        <img :src="annonce.photos[0].lienurl" alt="" class="deuxPhotosUn">
+        <img :src="annonce.photos[1].lienurl" alt="" class="deuxPhotosDeux">
+    </div>
+    <div v-else class="photos">
+        <img :src="annonce.photos[0].lienurl" alt="" class="troisPhotosUn">
+        <img :src="annonce.photos[1].lienurl" alt="" class="troisPhotosDeux">
+        <img :src="photo.lienUrlannonce.photos[2].lienurl" alt="" class="troisPhotosTrois">
+    </div> -->
 </template>
 <style scoped>
-
+.photos{
+    border-radius: 20px;
+    overflow: hidden;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+}
+.deuxPhotosUn, .deuxPhotosDeux{
+    height: auto;
+}
+.deuxPhotosUn{
+    padding-right: 5px;
+}
+.deuxPhotosDeux{
+    padding-left: 5px;
+}
 </style>
